2b5d7968036a9299db8484997fd7a198c2b3a69c
palmcivet7 <palmcivet@proton.me>
2024-07-21 10:19:08 +0100

    test merkleairdrop

--- merkle-airdrop/src/MerkleAirdrop.sol
+++ merkle-airdrop/src/MerkleAirdrop.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.24;
 
 import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import {console} from "forge-std/Test.sol";
 
 contract MerkleAirdrop {
     // some list of addresses
